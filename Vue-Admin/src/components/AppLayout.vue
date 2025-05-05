79c56e34d531b3414013635739192b2b51fd8bca
mahmdalqasm <mahmdalqasm12345@gmail.com>
2025-05-05 13:56:56 +0300

    make spinner component for use in many files in project

--- Vue-Admin/src/components/AppLayout.vue
+++ Vue-Admin/src/components/AppLayout.vue
@@ -11,20 +11,7 @@
         </div>
     </div>
     <div v-else class="min-h-full bg-gray-200 flex items-center justify-center">
-        <div class="flex flex-col items-center">
-            <svg
-                class="animate-spin h-16 w-16 text-gray-700 mr-2"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-            >
-                <path
-                    fill="currentColor"
-                    d="M12 2a1 1 0 0 1 1 1v3a1 1 0 0 1-2 0V3a1 1 0 0 1 1-1zm5.657 3.343a1 1 0 0 1 1.414 1.414L17.414 8.414a1 1 0 1 1-1.414-1.414l1.657-1.657zM21 11a1 1 0 1 1 0 2h-3a1 1 0 0 1 0-2h3zm-3.343 5.657a1 1 0 0 1-1.414 1.414L16.586 17.414a1 1 0 0 1 1.414-1.414l1.657 1.657zM13 21a1 1 0 0 1-2 0v-3a1 1 0 0 1 2 0v3zm-5.657-3.343a1 1 0 0 1-1.414-1.414L7.414 16.586a1 1 0 0 1 1.414 1.414L7.171 17.657zM3 13a1 1 0 0 1 0-2h3a1 1 0 0 1 0 2H3zm3.343-5.657A1 1 0 1 1 4.929 5.93l1.657 1.657A1 1 0 0 1 6.343 7.343z"
-                />
-            </svg>
-            <div class="mt-2 text-gray-800">Please Wait...</div>
-        </div>
+        <Spinner />
         </div>
 </template>
 
@@ -32,6 +19,7 @@
 import { ref, onMounted, onUnmounted, computed } from "vue";
 import Sidebar from "./Sidebar.vue";
 import Navbar from "./Navbar.vue";
+import Spinner from "./core/Spinner.vue";
 import { useUserStore } from "../stores/user";
 
 const userStore = useUserStore();
