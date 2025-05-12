6b21a65ff689b7b7ba8b3e612b956a8dadd6ca11
mahmdalqasm <mahmdalqasm12345@gmail.com>
2025-05-12 16:50:38 +0300

    Create Empty Product Modal

--- Vue-Admin/src/views/Products/Products.vue
+++ Vue-Admin/src/views/Products/Products.vue
@@ -2,19 +2,29 @@
     <div class="flex items-center justify-between mb-3">
         <h1 class="text-3xl font-semibold">Products</h1>
         <button
+            @click="openModal"
             type="submit"
             class="flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600"
         >
             Add New Product
         </button>
     </div>
+    <ProductsModal v-model="show" />
     <ProductsTable />
 
 
 </template>
 
 <script setup>
+import { ref } from 'vue';
+import ProductsModal from './ProductsModal.vue';
 import ProductsTable from './ProductsTable.vue';
+
+const show = ref(false)
+
+function openModal (){
+    show.value = true
+}
 
 
 </script>
